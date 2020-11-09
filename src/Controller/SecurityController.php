d0b55f3b0c99b71700f2d7be6921e7a212773597
sylpi <syl.pillet@hotmail.fr>
2020-11-09 17:20:59 +0100

    routing & link if connected of not

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,15 +14,14 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('account');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        $this->addFlash("success", "you are logged");
         
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
